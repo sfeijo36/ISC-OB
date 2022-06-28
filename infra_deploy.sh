4684607d6c540d05e3de9132af946d24d2195c71
sfeijo36 <sebafeijo19@gmail.com>
2022-06-28 01:33:40 -0300

    Update infra_deploy.sh

--- infra_deploy.sh
+++ infra_deploy.sh
@@ -6,7 +6,7 @@ terraform init
 terraform apply -auto-approve
 
 # Actuliazo y copio el archivo de config de kube.
-aws eks --region us-east-1 update-kubeconfig --name Cluster-Boutique     
+aws eks --region us-east-1 update-kubeconfig --name Cluster-Boutique
 cp /home/ec2-user/.kube/config /home/ec2-user/ISC-OB/deployment_servicios
 
 # Build y tag de docker + push hacia ECR
@@ -60,17 +60,17 @@ sudo su - root -c "docker tag adservice:latest $recommendationservice:latest"
 sudo su - root -c "docker push $recommendationservice:latest"
 
 #cd /home/ec2-user/ISC-OB/deployment_servicios
-#terraform init
-#terraform apply -auto-approve
+terraform init
+terraform apply -auto-approve
 
 # Obtengo la url del loadbalancer
-#url=$(kubectl get all -n boutique | grep -o .*.us-east-1.elb.amazonaws.com | awk '{print $NF}')
+url=$(kubectl get all -n boutique | grep -o .*.us-east-1.elb.amazonaws.com | awk '{print $NF}')
 
 # Instalacion del metrics server
-#kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
 # Instalacion del horizontal autoscaler sobre el frontend
-#kubectl autoscale deployment frontend -n boutique --cpu-percent=30 --min=1 --max=10
+kubectl autoscale deployment frontend -n boutique --cpu-percent=30 --min=1 --max=10
 
 # Genero tráfico sobre la página
 # kubectl run -i --tty load-generator --rm --image=busybox --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://$url; done"
